f86ee81929ae6a1f68af43df1ee6d017e2e1d7bc
Sairo Hernandez Alas <shernandez@grupocolono.com>
2020-10-27 19:59:05 -0600

    Carga Inicial

---
+++ Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PrograWeb.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Resultado</h3>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace PrograWeb
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+
+        MySqlConnection conn;
+        string myConnectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            conn = new MySqlConnection(myConnectionString);
+
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("select count(1) as cantidad from Usuarios");
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+            MySqlDataAdapter Da = new MySqlDataAdapter();
+            Da.SelectCommand = cmd;
+            Da.Fill(dt);
+
+            TextBox1.Text = dt.Rows[0][0].ToString();
+
+
+        }
+    }
+}

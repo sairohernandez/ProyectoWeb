1895e6fb71c213f88bd4c546130684cac007c67b
Agn75 <Alfredogn75@gmail.com>
2020-11-20 19:05:39 -0600

    Login

---
+++ Login.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="PrograWeb.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Label ID="Label_Username" runat="server" Text="Username"/>
+
+                <br />
+
+                <asp:TextBox ID="TextBox_Username" placeholder="Username" runat="server"/>
+
+                <br />
+
+                <asp:Label ID="Label_Password" runat="server" Text="Password"/>
+
+                <br />
+
+                <asp:TextBox ID="TextBox_Password" placeholder="Password" runat="server" TextMode="Password"/>
+
+                <br />
+
+                <asp:Button id="Submit" Onclick="Submit_Click" runat="server" Text="Login"/>
+
+                <br />
+
+                <asp:Label ID="Label_Error" runat="server" OnClick="Submit_Click" Text=""/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+using MySqlX.XDevAPI;
+
+namespace PrograWeb
+{
+    public partial class Login : System.Web.UI.Page
+
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+
+        {
+
+            MySqlConnection conn = new MySqlConnection("server=remotemysql.com;port=3306;database=JU7v5Dpmc3;user id=JU7v5Dpmc3; password=80JwZf6LER");
+
+            conn.Open();
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Usuarios where nombreUsuario = '" + TextBox_Username.Text + "' and claveUsuario = '" + TextBox_Password.Text + "'";
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Session["nombreUsuario"] = dr["nombreUsuario"].ToString();
+                Response.Redirect("Default.aspx");
+            }
+
+            conn.Close();
+
+            Label_Error.Text = "Username/Password Incorrect";
+
+            //String User = TextBox_Username.Text;
+            //String Password = TextBox_Password.Text;
+
+            //MySqlConnection conn = new MySqlConnection("server=remotemysql.com;port=3306;database=JU7v5Dpmc3;user id=JU7v5Dpmc3; password=80JwZf6LER") ;
+
+            //MySqlDataAdapter sda = new MySqlDataAdapter("select count (*) Usuarios where nombreUsuario = '" + TextBox_Username.Text + "' and claveUsuario = '"+ TextBox_Password.Text +"'", conn);
+
+            //DataTable dt = new DataTable();
+
+            //sda.Fill(dt);
+
+            //if (dt.Rows[0][0].ToString() == "1")
+            //{
+            //    Response.Redirect("Default.aspx");
+            //}
+            //else
+            //{
+            //    Label_Error.Text = "Username/Password Incorrect";
+            //}
+        }
+
+    }
+}
